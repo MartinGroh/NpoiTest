dedd5c13d4f0654801754acaf76d592b9560d4a7
Martin Groh <martin_groh@hotmail.com>
2018-10-15 22:15:43 +0200

    Add project files.

---
+++ NpoiTest/JalleData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NpoiTest
+{
+    public class JalleData
+    {
+        public int OneNumber { get; set; }
+        public string LittTekst { get; set; }
+        public double EtStortTall { get; set; }
+
+        public override string ToString()
+        {
+            return $"{OneNumber} / {LittTekst} / {EtStortTall}"; 
+        }
+    }
+}
---
+++ NpoiTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using Ganss.Excel;
+using Npoi.Mapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NpoiTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write();
+            Read();
+
+            Console.WriteLine("Enter to Exit");
+            Console.ReadLine();
+        }
+
+        private static void Read()
+        {
+            var mapper = new ExcelMapper(@"C:\temp\test.xls");
+            var data = mapper.Fetch<JalleData>("newSheet");//.Select(n=>n.Value);
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        private static void Write()
+        {
+            List<JalleData> data = new List<JalleData>();
+            data.Add(new JalleData() { EtStortTall = 234234.2342, LittTekst = "Hei paa meg", OneNumber = 27 });
+            data.Add(new JalleData() { EtStortTall = 7655.564, LittTekst = "Hei paa deg", OneNumber = 21 });
+
+            var mapper = new ExcelMapper();
+            mapper.Save(@"C:\temp\test.xls", data, "newSheet",false);
+        }
+    }
+}
